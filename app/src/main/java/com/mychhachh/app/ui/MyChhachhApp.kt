6c0fe2b2ef6929ee5cfdc61e7df7a8c23f480e0b
hazroinfo <124493545+hazroinfo@users.noreply.github.com>
2026-09-23 17:19:50 +0400

    Fix V95 drawer border import

--- app/src/main/java/com/mychhachh/app/ui/MyChhachhApp.kt
+++ app/src/main/java/com/mychhachh/app/ui/MyChhachhApp.kt
@@ -3,6 +3,7 @@ package com.mychhachh.app.ui
 import android.content.Intent
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
