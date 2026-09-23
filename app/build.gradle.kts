a5ed3f26a6a037d4c24dfac16b4636e65e402d66
hazroinfo <124493545+hazroinfo@users.noreply.github.com>
2026-09-23 12:50:14 +0400

    Bump exact website app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mychhachh.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 10
+        versionName = "2.0.0"
     }
 
     buildFeatures { compose = true }
