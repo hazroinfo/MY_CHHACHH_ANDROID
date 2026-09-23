12af9ec6946782bbe556fc9f9a7e8f30082d1103
hazroinfo <124493545+hazroinfo@users.noreply.github.com>
2026-09-23 19:24:21 +0400

    Fix V95 header Dp import

--- app/src/main/java/com/mychhachh/app/ui/MyChhachhApp.kt
+++ app/src/main/java/com/mychhachh/app/ui/MyChhachhApp.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.platform.LocalContext
 import coil.compose.AsyncImage
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.Dispatchers
