c298fb0c426b01435f2ef21632881bcbe73d6d51
hazroinfo <124493545+hazroinfo@users.noreply.github.com>
2026-09-26 15:55:13 +0400

    Bump smooth WebView to 2.0.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mychhachh.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "2.0.20"
+        versionCode = 29
+        versionName = "2.0.21"
     }
 
     compileOptions {
