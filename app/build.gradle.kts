26012f9b0ae1162414cbe536fba074b21bdd91a2
hazroinfo <124493545+hazroinfo@users.noreply.github.com>
2026-09-26 16:04:18 +0400

    Bump WebView rollback fix to 2.0.22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mychhachh.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "2.0.21"
+        versionCode = 30
+        versionName = "2.0.22"
     }
 
     compileOptions {
