8a4960ce3acc9617d55f69edd8abf8164bd9cc56
hazroinfo <124493545+hazroinfo@users.noreply.github.com>
2026-09-26 11:11:29 +0400

    Prepare fullscreen TWA app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mychhachh.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "2.1.0"
+        versionCode = 26
+        versionName = "2.2.0"
     }
 
     compileOptions {
